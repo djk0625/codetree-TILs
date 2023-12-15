b2aca21db021a14477790c159031417c10ca4bb2
Dong Ju Kim <53804133+djk0625@users.noreply.github.com>
2023-12-16 00:13:11 +0900

    Title: 중앙값 구하기, Time: 170ms, Memory: 24MB, Status: Passed - CodeTree

--- 231216/중앙값 구하기/find-the-median.py
+++ 231216/중앙값 구하기/find-the-median.py
@@ -1,8 +1,8 @@
 a, b, c = map(int, input().split())
 
-if a >= b and c >= a:
+if (a >= b and c >= a) or (a >= c and b >= a):
     print(a)
-elif b >= a and c >= b:
+elif (b >= a and c >= b) or (a >= b and b >= c):
     print(b)
-else:
+elif (c >= a and b >= c ) or (c >= b and a >= c):
     print(c)
