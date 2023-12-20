a951c88540fe390ebf2a446f176f993b85f6050a
Dong Ju Kim <53804133+djk0625@users.noreply.github.com>
2023-12-20 16:09:48 +0900

    Title: 369 게임, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231220/369 게임/369-game.py
@@ -0,0 +1,10 @@
+n = int(input())
+answer = []
+
+for i in range(1, n+1):
+    if(i % 3 == 0 or i % 10 == 3 or i % 10 == 6 or i % 10 == 9):
+        answer.append(0)
+    else:
+        answer.append(i)
+
+print(*answer)
