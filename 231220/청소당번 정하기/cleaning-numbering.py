1fbadaa9514a3cb97fc5c2f1674bbaaa80caa6b7
Dong Ju Kim <53804133+djk0625@users.noreply.github.com>
2023-12-20 16:58:09 +0900

    Title: 청소당번 정하기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231220/청소당번 정하기/cleaning-numbering.py
+++ 231220/청소당번 정하기/cleaning-numbering.py
@@ -7,11 +7,12 @@ c = 0
 for i in range(1, n+1):
     if i % 10 == 0:
         continue
+
     if i % 2 == 0:
         if i % 3 == 0:
-            b += 1
             if i % 12 == 0:
                 c += 1
+            b += 1
         if i % 12 == 0:
             c += 1
         a += 1
