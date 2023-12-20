026941a7408f0ab078ebb88c5d701465af02acbf
Dong Ju Kim <53804133+djk0625@users.noreply.github.com>
2023-12-20 17:49:17 +0900

    Title: 윤년 횟수 구하기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231220/윤년 횟수 구하기/number-of-leap-years.py
+++ 231220/윤년 횟수 구하기/number-of-leap-years.py
@@ -6,9 +6,8 @@ for i in range(1, n+1):
         answer += 1
     
     if i % 4 == 0:
+        if i % 100 == 0 and i % 400 != 0:
+            answer -= 1
         answer += 1
-    
-    if i % 100 == 0 and i % 400 != 0:
-        answer -= 1
-
+        
 print(answer)
